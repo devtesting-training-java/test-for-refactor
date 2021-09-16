0802cb3348e8b26c33b1dcacb9caef79f86f26a2
Vic Wu <madcocomo@gmail.com>
2021-09-16 11:31:33 +0800

    add code to refactor

--- src/main/java/io/xp/kata/Main.java
+++ src/main/java/io/xp/kata/Main.java
@@ -1,6 +1,52 @@
 package io.xp.kata;
 
+import io.xp.kata.gildedrose.GildedRose;
+import io.xp.kata.gildedrose.Item;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Main {
+
     public static void main(String[] args) {
+        String output = generateOutput(args);
+        System.out.print(output);
+    }
+
+    public static String generateOutput(String[] args) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        printStream.println("OMGHAI!");
+
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 2, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 3;
+        if (args.length > 0) {
+            days = Integer.parseInt(args[0]) + 1;
+        }
+
+        for (int i = 0; i < days; i++) {
+            printStream.println("-------- day " + i + " --------");
+            printStream.println("name, sellIn, quality");
+            for (Item item : items) {
+                printStream.println(item);
+            }
+            printStream.println();
+            app.update_quality();
+        }
+        return outputStream.toString();
     }
 }
---
+++ src/test/java/io/xp/kata/gildedrose/SafeNetTest.java
@@ -0,0 +1,22 @@
+package io.xp.kata.gildedrose;
+
+import io.xp.kata.Main;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class SafeNetTest {
+    @Test
+    public void testOutput() throws URISyntaxException, IOException {
+        String output = Main.generateOutput(new String[0]);
+        Path path = Paths.get(this.getClass().getResource("/baseline.txt").toURI());
+        String baseline = new String(Files.readAllBytes(path));
+        assertEquals(baseline, output);
+    }
+}

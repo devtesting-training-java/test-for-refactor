bbbf79808708284b82d50382cf888aaa74687049
Vic Wu <madcocomo@gmail.com>
2021-11-06 23:38:20 +0800

    improve test by test design

--- src/main/java/io/xp/kata/Main.java
+++ src/main/java/io/xp/kata/Main.java
@@ -18,21 +18,33 @@ public class Main {
         printStream.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new NormalItem("+5 Dexterity Vest", 10, 20), //
-                new AgedBrie("Aged Brie", 2, 0), //
-                new NormalItem("Elixir of the Mongoose", 2, 7), //
-                new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Passes("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new NormalItem("+5 Dexterity Vest", 10, 2),
+                new NormalItem("+5 Dexterity Vest", 10, 0),
+                new NormalItem("+5 Dexterity Vest", 0, 2),
+                new NormalItem("+5 Dexterity Vest", 0, 1),
+                new AgedBrie("Aged Brie", 10, 50),
+                new AgedBrie("Aged Brie", 10, 2),
+                new AgedBrie("Aged Brie", 0, 49),
+                new AgedBrie("Aged Brie", 0, 2),
+                new Sulfuras("Sulfuras, Hand of Ragnaros", 10, 2),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 11, 50),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 11, 2),
                 new Passes("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Passes("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Passes("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 10, 2),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 6, 49),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 6, 2),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 5, 48),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 5, 2),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 1, 48),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 1, 2),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+                new Passes("Backstage passes to a TAFKAL80ETC concert", 0, 2),
                 // this conjured item does not work properly yet
                 new NormalItem("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
-        int days = 3;
+        int days = 2;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }

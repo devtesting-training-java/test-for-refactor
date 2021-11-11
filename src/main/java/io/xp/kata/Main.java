d99e267f7700af38a98c667d576aa74ed2277172
Vic Wu <madcocomo@gmail.com>
2021-11-11 11:08:35 +0800

    remove test data

--- src/main/java/io/xp/kata/Main.java
+++ src/main/java/io/xp/kata/Main.java
@@ -23,11 +23,7 @@ public class Main {
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 2, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
